cda633b0f0ec52e7e4640e8455cc273b394fd058
ilanh <140815956+ilanhourchani@users.noreply.github.com>
2023-07-30 23:18:33 +0000

    Update bank_customer_churn_analysis.py

--- bank_customer_churn_analysis.py
+++ bank_customer_churn_analysis.py
@@ -1,5 +1,7 @@
 # Our Project Description
-As commonly understood, acquiring a new customer incurs higher costs compared to retaining an existing one. Hence, it is beneficial for banks to understand the reasons that prompt a customer to terminate their association with the institution. By preventing customer churn, companies can design loyalty programs and retention initiatives aimed at retaining a maximum number of customers.
+As commonly understood, acquiring a new customer incurs higher costs compared to retaining an existing one.
+Hence, it is beneficial for banks to understand the reasons that prompt a customer to terminate their association with the institution.
+By preventing customer churn, companies can design loyalty programs and retention initiatives aimed at retaining a maximum number of customers.
 
 - Analysis of Bank customers information to try to predict the profile of people that will churn
 - For the students, it allows us to better understand the banking system and customer information
